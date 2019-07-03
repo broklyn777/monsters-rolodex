91b574b23984b67d280992a1979f0105705dd5cf
Björn Jansson <bjornjansson@Bjorns-iMac.local>
2019-07-03 11:01:15 +0200

    Updates

---
+++ precache-manifest.6742120634ad8fa942c786edff848e49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49905b519890a05363466b1b918661d1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "15d294ff3d3c9d717535",
+    "url": "/monsters-rolodex/static/css/main.3cc64e3a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "15d294ff3d3c9d717535",
+    "url": "/monsters-rolodex/static/js/main.a96a0e77.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
